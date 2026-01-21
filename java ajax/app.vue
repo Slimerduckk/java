d6436e91b6365da70bf4b6652c1ff8cf75d97e5e
Sylviia <platon.parv@tptlive.ee>
2026-01-21 13:05:30 +0200

    Add files via upload

---
+++ java ajax/app.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue';
+
+    let message = ref('Hello Vue!');
+</script>
+
+<template>
+    <div class = "container">
+        <h1>{{message}}</h>
+        <button class ="button is-primary" v-on:click="message = 'Hello TA-24A!'"> Click me!</button>
+        <input class = "input" v-bind:value ="message" v-on:input="input">
+    </div>
+</template>
+
+<style>
+
+</style>
